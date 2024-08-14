5ef3b6a58024dbc664e8f16fd61839dfc6ed524a
João Gabriel Amaro <802.326@alunos.unigran.br>
2024-08-14 17:51:35 -0400

    ultimos ajustes para o modulo 1

--- frontend-web/src/components/Form.jsx
+++ frontend-web/src/components/Form.jsx
@@ -5,31 +5,26 @@ import { ACCESS_TOKEN,REFRESH_TOKEN } from "../constants";
 import LoadingIndicator from "./LoadingIndicator";
 import "../styles/Form.css"
 
-function Form({route, method}){
+function Form(){
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState("");
     const navigate = useNavigate();
-
-    const name = method === "login" ? "Login" : "Registrar"
 
         const handleSubmit = async (e) => {
         e.preventDefault();
         setLoading(true);
 
         try {
-            const res = await api.post(route, { username, password }, {
+            const res = await api.post('/api/token/', { username, password }, {
                 headers: {
                     'Client-Type': 'web'
                 }
             });
-            if (method === "login") {
-                localStorage.setItem(ACCESS_TOKEN, res.data.access);
-                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
-                navigate("/");
-            } else {
-                navigate("/login");
-            }
+
+            localStorage.setItem(ACCESS_TOKEN, res.data.access);
+            localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+            navigate("/");
         } catch (error) {
             if (error.response && error.response.data && error.response.data.detail) {
                 alert(error.response.data.detail);
@@ -41,11 +36,11 @@ function Form({route, method}){
         }
     };
     return <form onSubmit={handleSubmit} className="form-container">
-        <h1>{name}</h1>
+        <h1 className="title-login">Login</h1>
         <input className="form-input" type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Usuario"/>
         <input className="form-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Senha"/>
         {loading && <LoadingIndicator />}
-        <button className="form-button" type="submit">{name}</button>
+        <button className="form-button" type="submit">Login</button>
     </form>
 }
 
